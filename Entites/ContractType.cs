54d058db508902293bf2dd373f586ba678ca7468
Gabriel Alves <g.rodriguesalves72@gmail.com>
2024-10-08 18:49:59 -0300

    feat(Entites): creates the Entites classes to interface with the database entity.

--- Entites/ContractType.cs
+++
@@ -1,9 +0,0 @@
-﻿// Entities/ContractType.cs
-namespace doQrApi.Entities
-{
-    public class ContractType // Ensure this is a class, not a struct
-    {
-        public int ContractTypeId { get; set; } // Primary key
-        public string? ContractTypeName { get; set; } // Name of the contract type
-    }
-}
