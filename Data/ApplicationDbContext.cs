937c92110f76ff4b560e26576c33ddf502ae74f4
Gabriel Alves <g.rodriguesalves72@gmail.com>
2024-10-08 18:38:07 -0300

    feat(Data): create a ddd folder to configure database.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using doQrApi.Entites;
+using doQrApi.Entities;
+using doQrApi.Objects;
+using Microsoft.EntityFrameworkCore;
+
+namespace doQrApi.Data
+{
+#pragma warning disable CS1591
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Employee> TB_Employees {  get; set; }
+        public DbSet<ContractType> TB_contracttypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+#pragma warning restore CS1591
+
+}
